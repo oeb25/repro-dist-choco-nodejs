e619a91cf8309f8c7f6996e04cdef98c735ed0e9
oeb25 <oliverboving@gmail.com>
2025-03-19 15:19:34 +0100

    add debug printing

--- build.rs
+++ build.rs
@@ -1,10 +1,13 @@
 fn main() {
-    match list_program_files() {
-        Ok(_) => {}
-        Err(err) => {
-            eprintln!("failed to list program files: {}", err);
-        }
+    if let Err(err) = list_program_files() {
+        eprintln!("failed to list program files: {}", err);
     }
+    if let Err(err) = list_node_js_files() {
+        eprintln!("failed to list nodejs files: {}", err);
+    }
+    // print path
+    let path = std::env::var("PATH").expect("PATH environment variable is not set");
+    eprintln!("PATH: {:?}", path);
 
     // get npm version
     let npm_version = std::process::Command::new("npm")
@@ -27,6 +30,18 @@ fn list_program_files() -> Result<(), String> {
         let entry = entry.map_err(|err| format!("failed to read entry: {}", err))?;
         let path = entry.path();
         eprintln!("program file: {}", path.display());
+    }
+    Ok(())
+}
+
+fn list_node_js_files() -> Result<(), String> {
+    let path = "C:\\Program Files\\nodejs";
+    let node_js_files =
+        std::fs::read_dir(path).map_err(|err| format!("failed to read {}: {}", path, err))?;
+    for entry in node_js_files {
+        let entry = entry.map_err(|err| format!("failed to read entry: {}", err))?;
+        let path = entry.path();
+        eprintln!("nodejs file: {}", path.display());
     }
     Ok(())
 }
